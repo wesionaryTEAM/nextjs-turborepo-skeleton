d073a8fed9d922b95816ad4d63a6c6dbeaf9598f
Rasil07 <rasilgrt@gmail.com>
2022-06-28 13:52:57 +0545

    feat: configured packages / ui to use tailwindcss and enabled apps to use ui components

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,7 @@
-import * as React from 'react';
 export const Button = () => {
-  return <button>Boop</button>;
+  return (
+    <button className="bg-indigo-500 text-base rounded-full px-4  py-0.5 text-white">
+      Button
+    </button>
+  );
 };
