db3a0f0e17dc80fca4eed0166c7bcc0203ffe71f
Rasil07 <rasilgrt@gmail.com>
2022-06-29 11:19:04 +0545

    chore: added env.exampole

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   printWidth: 80,
-  semi: true,
+  semi: false,
   singleQuote: true,
   tabWidth: 2,
   useTabs: false,
--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -3,5 +3,5 @@ export const Button = () => {
     <button className="bg-indigo-500 text-base rounded-full px-4  py-0.5 text-white">
       Button
     </button>
-  );
-};
+  )
+}
