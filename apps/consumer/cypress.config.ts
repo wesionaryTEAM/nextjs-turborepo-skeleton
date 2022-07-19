f520cbf9863f410ad014222401e9e46e9e7b0b33
bigyanpoudel <46457363+bigyanpoudel@users.noreply.github.com>
2022-07-19 10:30:31 +0545

    cypress config

--- apps/consumer/cypress.config.ts
+++ apps/consumer/cypress.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'cypress'
+import * as cypress from 'cypress'
 
-export default defineConfig({
+export default cypress.defineConfig({
   e2e: {
-    setupNodeEvents() {
+    setupNodeEvents(_, config: Cypress.PluginConfigOptions) {
       // implement node event listeners here
+      return {
+        ...config,
+        baseUrl: `${config.env.HOST}:${config.env.PORT}/`,
+      }
     },
   },
 })
